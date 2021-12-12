15fc63b0b32c7c62fdf316458ce75067e12ccebf
muluken-beep <60249295+muluken-beep@users.noreply.github.com>
2021-12-12 13:52:24 -0800

    Js states and event handlers

---
+++ Part1/Exercises 1.6-1.9/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const Display = ({counter}) => <div>{counter}</div> 
+
+
+const Button = ({onClick,text}) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+    )
+
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)  
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+  
+  return (
+    <div>
+     <Display counter={counter}/>
+      <Button onClick = {increaseByOne}
+        text= 'plus'
+       />    
+      <Button onClick={setToZero}
+        text ='reset'
+      /> 
+      <Button onClick={decreaseByOne}
+        text ='minus'
+      /> 
+    </div>
+  )
+}
+
+export default App;
